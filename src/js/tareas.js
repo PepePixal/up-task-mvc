0f82777f65730319292bd677edd68a84ec014187
PepePixal <pepepixal@gmail.com>
2025-04-25 12:21:33 +0200

    modal finalizada hasta vid.615

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -30,18 +30,42 @@
                     <input type="submit" class="submit-nueva-tarea" value="Agregar Tarea" />
                     <button type="button" class="cerrar-modal">Cancelar</button>
                 </div>
-            </form>;
+            </form>
         `;
         
+        //En JavaScript el setTimeout() es lo último que se ejecuta dentro de la función.
         //agrega la clase animar al formulario a los x segundos,
-        //clase que hace que se muestre la ventana modal, con scss
+        //clase que hace que se muestre la ventana modal, según su animacion css
         setTimeout(() => {
             //selecciona el elemento con clase .formulari y lo asigna a formulario
             const formulario = document.querySelector('.formulario');
             //agrega la clase animar al elemento formulario
             formulario.classList.add('animar');
         }, 0);
+        
+        //como modal es un elemento html (div), podemos asignarle un evento
+        modal.addEventListener('click', function(e) {
+            //por defecto el input tipo submit envia formulario y cierra la ventana modal,
+            //para prevenir las acciones por defecto del evento en e:
+            e.preventDefault();
 
+            //En JS delegation, es conocer sobre que elemento se está ejecutando el evento.
+            //Si el elemento que genera el evento (que obtenemos de e.target), 
+            //contiene la class 'cerrar-modal' (en este caso el boton cerrar)
+            if(e.target.classList.contains('cerrar-modal')){
+                //selecciona el elemento con la clase .formulario y lo asigna a formulario
+                const formulario = document.querySelector('.formulario');
+                //agrega la clase cerrar al elemento formulario, para darle estilo css
+                formulario.classList.add('cerrar');
+                //usamos el setTiemout() para que se ejecute lo último
+                setTimeout(() => {
+                    //elimina el div de modal, por tanto, cierra la ventana modal,
+                    modal.remove();
+                }, 500);
+            }
+        })
+
+        //imprimir la ventana modal
         //selecciona el elemento <body> del dom y le agrega el código html de la var modal 
         document.querySelector('body').appendChild(modal);
         
